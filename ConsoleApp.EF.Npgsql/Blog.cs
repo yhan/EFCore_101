9f12257f26e04bfd8bfdae1cc58828513a705182
Yi Han <hanyi@8110@gmail.com>
2021-02-26 17:07:47 +0100

    Add Blog CreatedTimestamp

--- ConsoleApp.EF.Npgsql/Blog.cs
+++ ConsoleApp.EF.Npgsql/Blog.cs
@@ -1,8 +1,11 @@
-﻿namespace ConsoleApp.EF.Npgsql
+﻿using System;
+
+namespace ConsoleApp.EF.Npgsql
 {
     public class Blog
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public DateTime CreatedTimestamp { get; set; }
     }
 }
--- ConsoleApp.EF.Npgsql/Migrations/20210226160420_AddBlogCreatedTimestamp.Designer.cs
+++ ConsoleApp.EF.Npgsql/Migrations/20210226160420_AddBlogCreatedTimestamp.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using ConsoleApp.EF.Npgsql;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace ConsoleApp.EF.Npgsql.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20210226160420_AddBlogCreatedTimestamp")]
+    partial class AddBlogCreatedTimestamp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
