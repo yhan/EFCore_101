0e97df96f91679748a880aaa1114665471d9a8c6
Yi Han <hanyi@8110@gmail.com>
2021-02-28 15:41:27 +0100

    try add ef console logging dbug: Microsoft.EntityFrameworkCore.Infrastructure[10409]       An additional 'IServiceProvider' was created for internal use by Entity Framework. An existing service provider was not used due to the following configuration changes: configuration added for 'Core:UseLoggerFactory', configuration added for 'Core:UseMemoryCache', configuration added for 'Core:EnableSensitiveDataLogging', configuration added for 'Core:EnableDetailedErrors', configuration added for 'Core:ConfigureWarnings', configuration added for 'Devart.Data.PostgreSql.Entity.PgSqlOptionsExtension'. info: Microsoft.EntityFrameworkCore.Infrastructure[10403]       Entity Framework Core 2.2.4-servicing-10062 initialized 'MyContext' using provider 'Devart.Data.PostgreSql.Entity.EFCore' with options: None

--- ConsoleApp.EF/MyContext.cs
+++ ConsoleApp.EF/MyContext.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Options;
 
 namespace ConsoleApp.EF
 {
     public class MyContext : DbContext
     {
+        private readonly ServiceProvider _serviceProvider;
+        private static readonly string ConnectionString = $"User Id=postgres;Password=root;Host=localhost;Database={DbName};Port=5433;Persist Security Info=True;Initial Schema={Schema};Unicode=True;License Key=wMnQAqtt0hxxsDqJWvW9KxIj97Gok/73AlbVrLT0DD/uMFfe0qmajvZ7hwGLdgDsU0YXAbKCsQdefn/Tk2w4hP6ayVO14kefgaCTThN0Pserh6XISmNJiiEYrDxw/mlJfIbmJZovZts6ABoyLNOe24T/xWFPBVcL/dA8irfcBzxMXYEdH/IHfWenC2oN4DOh7lPTxS8zLJP7KXn1u85/8B6VoQtWnxcwDcLSKYfXHZGoVidjLYm8LiWYB7hTGU3dgx0AQsbDJN1CVVvXyC9BA/mWxvV+LRnXRYzr5jTW50U=";
+
+
         public DbSet<OrderDetail> OrderDetails { get; set; }
         public DbSet<Order> Orders { get; set; }
 
@@ -11,12 +19,20 @@ namespace ConsoleApp.EF
         private const string Schema = "public";
 
         // Min Pool Size=0;Max Pool Size=100;
-        private static readonly string ConnectionString = $"User Id=postgres;Password=root;Host=localhost;Database={DbName};Port=5433;Persist Security Info=True;Initial Schema={Schema};Unicode=True;License Key=wMnQAqtt0hxxsDqJWvW9KxIj97Gok/73AlbVrLT0DD/uMFfe0qmajvZ7hwGLdgDsU0YXAbKCsQdefn/Tk2w4hP6ayVO14kefgaCTThN0Pserh6XISmNJiiEYrDxw/mlJfIbmJZovZts6ABoyLNOe24T/xWFPBVcL/dA8irfcBzxMXYEdH/IHfWenC2oN4DOh7lPTxS8zLJP7KXn1u85/8B6VoQtWnxcwDcLSKYfXHZGoVidjLYm8LiWYB7hTGU3dgx0AQsbDJN1CVVvXyC9BA/mWxvV+LRnXRYzr5jTW50U=";
         
+
+        public MyContext(ServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UsePostgreSql(ConnectionString);
+            var optionsMonitor = _serviceProvider.GetService<IOptionsMonitor<ConsoleLoggerOptions>>();
+            optionsBuilder.UseLoggerFactory(new LoggerFactory(new []
+            {
+                new ConsoleLoggerProvider(optionsMonitor)
+            })).UsePostgreSql(ConnectionString);
         }
     }
 }
--- ConsoleApp.EF/Program.cs
+++ ConsoleApp.EF/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace ConsoleApp.EF
 {
@@ -7,13 +11,44 @@ namespace ConsoleApp.EF
     {
         static void Main(string[] args)
         {
-            var context = new MyContext();
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            var serviceProvider = ConfigureServices(config)
+                .BuildServiceProvider();
+
+            var context = new MyContext(serviceProvider);
             foreach (var orderDetail in context.OrderDetails)
             {
-                
+
             }
-           
+
             Console.WriteLine("Hello World!");
         }
+
+
+        private static IServiceCollection ConfigureServices(IConfiguration config)
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddLogging(builder =>
+            {
+                builder.AddConsole();
+                builder.AddConfiguration(config.GetSection("Logging"));
+            });
+
+            return services;
+        }
+
+        private static ILoggerFactory GetLoggerFactory()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder =>
+                builder.AddConsole());
+
+            return serviceCollection.BuildServiceProvider()
+                .GetService<ILoggerFactory>();
+        }
+
     }
 }
