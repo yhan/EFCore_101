b0b75c4e79d35615b2c7642dc7370ef8e662c2d6
Yi Han <hanyi@8110@gmail.com>
2021-02-28 14:32:43 +0100

    Seperate file

--- ConsoleApp.EF/Program.cs
+++ ConsoleApp.EF/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
 
 namespace ConsoleApp.EF
 {
@@ -15,25 +14,6 @@ namespace ConsoleApp.EF
             }
            
             Console.WriteLine("Hello World!");
-        }
-    }
-
-
-    public class MyContext : DbContext
-    {
-        public DbSet<OrderDetail> OrderDetails { get; set; }
-        public DbSet<Order> Orders { get; set; }
-
-        private const string DbName = "StoreDB";
-        private const string Schema = "public";
-
-        // Min Pool Size=0;Max Pool Size=100;
-        private static readonly string ConnectionString = $"User Id=postgres;Password=root;Host=localhost;Database={DbName};Port=5433;Persist Security Info=True;Initial Schema={Schema};Unicode=True;License Key=wMnQAqtt0hxxsDqJWvW9KxIj97Gok/73AlbVrLT0DD/uMFfe0qmajvZ7hwGLdgDsU0YXAbKCsQdefn/Tk2w4hP6ayVO14kefgaCTThN0Pserh6XISmNJiiEYrDxw/mlJfIbmJZovZts6ABoyLNOe24T/xWFPBVcL/dA8irfcBzxMXYEdH/IHfWenC2oN4DOh7lPTxS8zLJP7KXn1u85/8B6VoQtWnxcwDcLSKYfXHZGoVidjLYm8LiWYB7hTGU3dgx0AQsbDJN1CVVvXyC9BA/mWxvV+LRnXRYzr5jTW50U=";
-        
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UsePostgreSql(ConnectionString);
         }
     }
 }
